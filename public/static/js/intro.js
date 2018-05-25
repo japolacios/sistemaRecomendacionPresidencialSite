3a01113ac34bfbcfa4083f0a322773c712bf876d
Juan Cuadros <juan.josecuadros1@gmail.com>
2018-05-25 14:20:49 -0500

    pantalla inicio> pantalla info demografica> primera pregunta

--- public/static/js/intro.js
+++ public/static/js/intro.js
@@ -4,6 +4,8 @@ function introduccion() {
     document.querySelector("#zona-pregunta").style.display = "none";
     document.querySelector("#si").style.display = "none";
     document.querySelector("#no").style.display = "none";
+    document.querySelector("#formulario-demografico").style.display = "none";
+    document.querySelector("#continuar-formulario").style.display = "none";
 }
 
 document.querySelector('#continuar').addEventListener('click', iniciar);
@@ -13,10 +15,22 @@ document.querySelector('#continuar').addEventListener('click', iniciar);
 function iniciar(e) {
     console.log('empezaste el quiz papulardo');
     e.preventDefault();
-    empezarPreguntas();
+    // empezarPreguntas();
+    empezarDemografico();
 }
 
+function empezarDemografico() {
+    document.querySelector("#zona-texto-intro").style.display = "none";
+    document.querySelector('#continuar').style.display = "none";
+    document.querySelector("#formulario-demografico").style.display = "block";
+    document.querySelector("#continuar-formulario").style.display = "block";
+}
+
+document.querySelector('#continuar-formulario').addEventListener('click', empezarPreguntas);
+
 function empezarPreguntas() {
+    document.querySelector("#formulario-demografico").style.display = "none";
+    document.querySelector("#continuar-formulario").style.display = "none";
     document.querySelector("#zona-texto-intro").style.display = "none";
     document.querySelector("#zona-pregunta").style.display = "block";
     document.querySelector("#si").style.display = "inline-block";
