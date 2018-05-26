9c38e54aa77f92a9e86bdd842c323a5565509bbd
Japo <japolacios@gmail.com>
2018-05-25 20:06:15 -0500

    Merge por terceros con public de cuadros

--- public/static/js/loader.js
+++ public/static/js/loader.js
@@ -1,5 +1,10 @@
 //ArrayList Donde se guardan las preguntas para recomendacion
 var pollQuestionsProcesed = [];
+var contador = 0;
+
+var botonSi = document.querySelector("#si");
+var botonNo = document.querySelector("#no");
+var botonContinuarFormulario = document.querySelector("#continuar-formulario");
 
 $(document).ready(function () {
     //Gets the RAW JSON
@@ -25,7 +30,7 @@ $(document).ready(function () {
                 duque: tempData.duque,
             });
         }
-        console.log(pollQuestionsProcesed);
+        // console.log(pollQuestionsProcesed);
         //addToDom();
     });
 
@@ -45,11 +50,36 @@ addToDom = function () {
         var fajardo = pollQuestionsProcesed[i].fajardo;
         var petro = pollQuestionsProcesed[i].petro;
         var duque = pollQuestionsProcesed[i].duque;
-        var tempHtml = "<ul><li>Pregunta: " + pregunta + "</li><li>vargas: " + vargas + "</li><li>calle: " + calle + "</li><li>Fajardo: " + fajardo + "</li><li>Petro: " + petro + "</li><li>Duque: " + duque + "</li></ul>"
+        var tempHtml = "<ul><li>Pregunta: " + pregunta + "</li><li>vargas: " + vargas + "</li><li>calle: " + calle + "</li><li>Fajardo: " + fajardo + "</li><li>Petro: " + petro + "</li><li>Duque: " + s + "</li></ul>"
 
-        var tempHtmlDos = "<div><p>" + "Introduccion" + "</div></p>"
         console.log(tempHtml);
         //El innerHtml remplaza lo que tenga previamente el elemento (body para este caso), por ende solo veran la lista de los ultimos del ciclo. Para hacer buen uso de eso les recomiendo utilizar los appends y no innerHtml, pero ya es el gusto de cada quien.
         document.getElementById("domBody").innerHTML = tempHtml;
+    }
+}
+
+botonSi.addEventListener('click', undisteSisas);
+botonNo.addEventListener('click', undisteNonas);
+botonContinuarFormulario.addEventListener('click', appendPregunta);
+
+
+function undisteSisas() {
+    console.log("undiste sisas");
+}
+
+function undisteNonas() {
+    console.log("undiste nonas");
+}
+
+function appendPregunta() {
+    console.log("pinte la primera pregunta");
+
+    if (contador <= pollQuestionsProcesed.length - 1) {
+        console.log(pollQuestionsProcesed[contador].pregunta);
+
+        var p = document.createElement("p");
+        var h = document.createTextNode(pollQuestionsProcesed[contador].pregunta);
+        document.getElementById("pregunta").appendChild(h);
+        // p.appendChild("<p>" + h + "</p>");
     }
 }
